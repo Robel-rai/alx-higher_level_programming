1b4830eb42a19dba6c8eda624bff8987554f6450
Robel-rai <robelabera02@gmail.com>
2022-11-30 12:04:50 -0800

    Another edit on Q9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3    
-def print_last_digit(number):
-    print(abs(number) % 10, end="")
-    return (abs(number) % 10)
+#!/usr/bin/python3
+def print_last_digit(i):
+    print(abs(i) % 10, end="")
+    return (abs(i) % 10)
