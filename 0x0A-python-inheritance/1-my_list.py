15f070d3c854ad84cfd4e90838a40a96b5bc8201
Robel-rai <robelabera02@gmail.com>
2023-01-09 13:15:52 -0800

    All the documentation for test and py tasts

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defined a class that inherits from list and pass to sort a list."""
+
+class MyList(list):
+    """ shoring algotithem using the sort function"""
+
+    def print_sorted(self):
+        """Print the sorted data in the assending format"""
+        print(sorted(self))
+
---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Define a function that can check if the class instance are the same"""
+
+def is_same_class(obj, a_class):
+        """Check if an object is exactly an instance of a given class.
+    Args:
+        obj (any): The object to check.
+        a_class (type): The class to match the type of obj to.
+    Returns:
+        If obj is exactly an instance of a_class - True.
+        Otherwise - False.
+    """
+    if type(obj) == a_class:
+        return True
+    return False
