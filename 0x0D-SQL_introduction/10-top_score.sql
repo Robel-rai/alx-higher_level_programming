0359e44676d350564891702dcdf65c4848090d79
Robel-rai <robelabera02@gmail.com>
2023-03-17 06:25:40 -0700

    Yahooo question 9 - 13

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,2 @@
+-- Listes all the names and the score of the user in the Desinding order.
+SELECT score, name FROM second_table ORDER BY score DESC;
---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,2 @@
+-- Listes all the names and the score of the user in the Desinding order.
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
