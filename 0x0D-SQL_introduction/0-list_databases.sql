61e5b300b126597b6323005f52360b7887be7772
Robel-rai <robelabera02@gmail.com>
2023-03-17 03:26:51 -0700

    Trial

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,7 +1,1 @@
-Enter password: 
-Database                                                                                     
-hbtn_0c_0                                                                                    
-information_schema                                                                           
-mysql                                                                                        
-performance_schema                                                                           
-sys   
+mysql -u root -p
