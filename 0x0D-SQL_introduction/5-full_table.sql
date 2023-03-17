3ec54a3c980284d3337d1d42205a6833691401b8
Robel-rai <robelabera02@gmail.com>
2023-03-17 04:55:52 -0700

    Question 4 and 5

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- a script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
+SELECT column_name, data_type, character_maximum_length
+FROM information_schema.columns
+WHERE table_name = 'first_table' AND table_schema = 'hbtn_0c_0';
