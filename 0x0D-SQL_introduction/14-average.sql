6ab5fac6bf2c06e19fd87dd1ede2e6f0b2c2e98a
Robel-rai <robelabera02@gmail.com>
2023-03-17 06:44:56 -0700

    ERe 0^0 Question correction for question 14 and 15

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,2 +1,2 @@
 -- Now we are going to be printing the avarage value of all the students.
-SELECT AVG(score) FROM second_table;
+SELECT AVG(score)AS average FROM second_table;
--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,4 @@
 -- listes the amount of number similar scores appered and diplays them in the given oreder.
-SELECT score, COUNT(*) AS number FROM hbtn_0c_0.second_table GROUP BY score ORDER BY number DESC;
+SELECT score, COUNT(*) AS number FROM second_table 
+GROUP BY score 
+ORDER BY number DESC;
