62e3e5c750be32f84a7e3734ba6feb345a5c448a
Robel-rai <robelabera02@gmail.com>
2023-03-20 02:51:08 -0700

    Hello 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,10 +9,10 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+    state_name = argv[4]
 
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=username, passwd=password,
