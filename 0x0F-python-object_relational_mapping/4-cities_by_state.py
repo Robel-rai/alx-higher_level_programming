6f5c234a89160fa81b6a11d1c9e22308ce56173f
Robel-rai <robelabera02@gmail.com>
2023-03-20 03:26:04 -0700

    number 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -4,12 +4,12 @@ lists all cities from the database hbtn_0e_4_usa
 """
 
 import MySQLdb
-from sys import argv
+import sys
 
 if __name__ == "__main__":
-    username = argv[1]
-    password = argv[2]
-    database = argv[3]
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=username, passwd=password,
